72a88018443200627af97a37dbcc9fa1f824390a
NaviHX <navihx@foxmail.com>
2024-10-04 17:42:12 +0800

    update(waiting_controller_manager): add event process logic

--- src/controller_manager/waiting_controller_manager.rs
+++ src/controller_manager/waiting_controller_manager.rs
@@ -1,36 +1,67 @@
-use std::{cell::RefCell, rc::Rc};
+use std::{cell::RefCell, collections::HashMap, rc::Rc};
 
 use anyhow::Result as Anyhow;
+use polling::Event;
 
 use crate::poll_manager::PollManager;
 
 use super::{controller::Controller, ControllerManager, ControllerMessage};
 
-pub struct WaitingControllerManager;
+pub struct WaitingControllerManager {
+    controllers: HashMap<usize, (usize, Rc<RefCell<Controller>>)>,
+}
 
 impl WaitingControllerManager {
     pub fn new() -> Self {
-        todo!()
+        Self {
+            controllers: HashMap::new(),
+        }
     }
 
     pub fn add_new_device(
-        &self,
-        _token: usize,
-        _controller: Rc<RefCell<Controller>>,
-        _poll_manager: &mut PollManager<ControllerManager, Anyhow<ControllerMessage>>,
+        &mut self,
+        token: usize,
+        controller: Rc<RefCell<Controller>>,
+        poll_manager: &mut PollManager<ControllerManager, Anyhow<ControllerMessage>>,
     ) -> Anyhow<()> {
-        todo!()
+        let callback = Box::new({
+            let controller = controller.clone();
+            move |_ctx: &mut ControllerManager| {
+                controller
+                    .borrow_mut()
+                    .handle_pairing_events()
+                    .map(|state| ControllerMessage::StateUpdate(token, state))
+            }
+        });
+
+        let callback_key = poll_manager.subscribe(
+            &*controller.borrow(),
+            Event::readable(0),
+            polling::PollMode::Level,
+            callback,
+        )?;
+        self.controllers.insert(token, (callback_key, controller));
+
+        Ok(())
     }
 
     pub fn remove_device(
-        &self,
-        _token: usize,
-        _poll_manager: &mut PollManager<ControllerManager, Anyhow<ControllerMessage>>,
+        &mut self,
+        token: usize,
+        poll_manager: &mut PollManager<ControllerManager, Anyhow<ControllerMessage>>,
     ) -> Anyhow<()> {
-        todo!()
+        let (callback_key, controller) = self
+            .controllers
+            .remove(&token)
+            .ok_or_else(|| anyhow::anyhow!("No controller for token {token} to remove"))?;
+        poll_manager.remove(callback_key, &*controller.borrow())?;
+        Ok(())
     }
 
     pub fn get_controller(&self, token: usize) -> Anyhow<Rc<RefCell<Controller>>> {
-        todo!()
+        self.controllers
+            .get(&token)
+            .ok_or_else(|| anyhow::anyhow!("No controller for token {token}"))
+            .map(|c| c.1.clone())
     }
 }
